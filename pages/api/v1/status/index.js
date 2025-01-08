7f6b7bb8e992c976e8366c00d3e01f5b4692e705
Ludimilla1325 <ludimillakrautzuk1325@gmail.com>
2025-01-07 22:30:37 -0300

    new commit

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,8 @@
 import database from '../../../../infra/database'
 
-function status(request, response){
+async function status(request, response){
+  const result = await database.query({text: 'SELECT NOW()'})
+  console.log(result.rows[0].now)
   response.status(200).json({"teste": "teste"})
 }
 
