d59f9b09c59d92bb082ff5cd0aef0bf7d4ecb485
Ludimilla1325 <ludimillakrautzuk1325@gmail.com>
2025-02-03 22:06:10 -0300

    make database.js more robust to error handling"

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from 'infra/database'
 
 async function status(request, response){
-  const result = await database.query({text: 'SELECT NOW()'})
-  console.log(result.rows[0].now)
-  response.status(200).json({"teste": "teste"})
+  const updatedAt = new Date().toISOString()
+  const resVersion = await database.query('SHOW server_version;') // select version();
+  const resMaxConections = await database.query('SHOW max_connections') //SELECT COUNT(*) FROM pg_stat_activity;
+  const databaseName =process.env.POSTGRES_DB
+  const resActiceConections = await database.query(
+    {text: `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1 AND state = 'active';`,
+    values: [databaseName]}
+  ) 
+  const version = resVersion.rows[0].server_version
+  const max_connections= resMaxConections.rows[0].max_connections
+  const active_connections = resActiceConections.rows[0].count
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    "dependencies":{
+      "database":{
+        version: version,
+        max_connections: Number(max_connections),
+        active_connections: active_connections
+      }
+    }
+  })
 }
 
-export default status
+export default status 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status")
   expect(response.status).toBe(200)
+
+  const responseBody = await response.json()
+  expect(responseBody.updated_at).toBeDefined()
+  expect(responseBody.dependencies.database.version).toBeDefined()
+  expect(responseBody.dependencies.database.version).toEqual("16.0")
+  expect(responseBody.dependencies.database.max_connections).toBeDefined()
+  expect(responseBody.dependencies.database.max_connections).toEqual(100)
+  expect(responseBody.dependencies.database.active_connections).toBeDefined()
+  expect(responseBody.dependencies.database.active_connections).toEqual(1)
+  expect(typeof responseBody.updated_at).toBe('string')
+  expect(typeof responseBody.dependencies.database.version).toBe('string')
+  expect(typeof responseBody.dependencies.database.max_connections).toBe('number')
+  expect(typeof responseBody.dependencies.database.active_connections).toBe('number')
+  expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(responseBody.dependencies.database.active_connections)
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString()
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt)
 });
