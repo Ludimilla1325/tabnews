152f2723c7fb65735e65420865b512b236af1e28
Ludimilla1325 <ludimillakrautzuk1325@gmail.com>
2025-02-04 21:48:46 -0300

    new updated

--- infra/database.js
+++ infra/database.js
@@ -6,14 +6,16 @@ async function query(queryObject){
     host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT
+    port: process.env.POSTGRES_PORT,
+    ssl: process.env.NODE_ENV === 'development' ? false : true
   })
-  await client.connect()
   try {
+    await client.connect()
     const result = await client.query(queryObject)
     return result
   } catch (error) {
     console.error('Error running query', error)
+    throw error
   } finally{
     await client.end()
   }
