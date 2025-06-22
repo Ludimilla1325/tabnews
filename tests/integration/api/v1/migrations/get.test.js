444bdae619fd79b9e0cbf5212587a86bbbe1f4dd
Ludimilla1325 <ludimillakrautzuk1325@gmail.com>
2025-06-22 19:43:24 -0300

    fix: tests

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,20 +1,19 @@
 import database from "infra/database";
 
 
-async function cleanDatabase(){
-  await database.query("drop schema public cascade; create schema public;")
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
 }
 
-beforeEach(cleanDatabase)
+beforeAll(cleanDatabase);
 
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations")
-  console.log('response', response);
+  console.log(response);
   
   expect(response.status).toBe(200)
   
   const responseBody = await response.json();
-  database.query('select 1+1')
 
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0)
